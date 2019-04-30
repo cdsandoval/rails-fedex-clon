f13761d40267c211dc4c5943c963686736b06088
Brayan Ciudad <linzeur@gmail.com>
2019-04-30 09:47:57 -0500

    Add validation to models (#28)
    * Create model
    
    * Add validations in models

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,9 @@
 class Shipment < ApplicationRecord
   belongs_to :user
   belongs_to :sender
+  has_many :shipment_locations
+
+  validates :origin_adress, :destination_address, presence: true
+  validates :weight, :freight_value, numericality: { greater_than: 0 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :shipments
+
+  validates :address, :city, :country, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
