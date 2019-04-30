40ca4fe872fe70ab85e1231043986762235dda94
Paul Portillo <yummta@gmail.com>
2019-04-30 10:35:05 -0500

    Add main routes (#29)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  resources :shipment, only: [:show]
+  get 'search', to: 'shipment#show'
+
+  devise_for :users
+
+  namespace :deposit do
+    root 'home#index'
+    get 'search', to: 'shipment#show'
+  end
+
+  namespace :admin do
+    root 'home#index'
+  end
+
+  namespace :api do
+  end
+
 end
