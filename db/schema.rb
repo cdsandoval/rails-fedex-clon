ea831e90a54f8eb579b8b14ce3cd647e1984a064
Carlos Sandoval <iznomag@gmail.com>
2019-05-03 10:00:05 -0500

    Fixing deposit policy error (#51)
    * Add two new columns to sender
    
    * Fixing deposit policy bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_220303) do
+ActiveRecord::Schema.define(version: 2019_05_03_030335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_04_30_220303) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country"
+    t.string "city"
   end
 
   create_table "shipment_locations", force: :cascade do |t|
